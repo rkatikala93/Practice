24a9fa5167ee44e0425cf4cd590f7515e5916096
rkatikala93 <renukadevi1993@gmail.com>
2022-02-01 15:25:51 -0600

    commiting to a new repo

---
+++ Rockpapperscissors.py
@@ -0,0 +1,13 @@
+import random
+computer_choice = random.choice(['rock', 'paper', 'scissors'])
+user_choice = input("Do you want - rock, paper, or scissors? \n")
+if computer_choice == user_choice:
+    print("Tie + computer choice is " + computer_choice)
+elif user_choice == 'rock' and computer_choice == 'scissors':
+    print("WIN + computer choice is " + computer_choice)
+elif user_choice == 'paper' and computer_choice == 'rock':
+    print("WIN + computer choice is " + computer_choice)
+elif user_choice == 'scissors' and computer_choice == 'paper':
+    print("WIN + computer choice is " + computer_choice)
+else:
+    print("You Lose (Computer Wins)! + computer choice is " + computer_choice)
