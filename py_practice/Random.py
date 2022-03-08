2f388df428cd8ece249733919e13904989e1bc7b
rkatikala93 <renukadevi1993@gmail.com>
2022-03-08 15:34:20 -0600

    random

--- py_practice/Random.py
+++ py_practice/Random.py
@@ -1,4 +1,6 @@
 import random
-diceroll = random.randint(1, 6)
-print("The computer rolled a " + str(diceroll))
+#diceroll = random.randint(1, 6)
+#print("The computer rolled a " + str(diceroll))
 
+selection = str(random.choice(['Rock', 'Paper', 'Scissors']))
+print("Computer selection is " + selection)
