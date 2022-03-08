b0126fbe38b65564b24c6c87e6970fe6837f2afd
rkatikala93 <renukadevi1993@gmail.com>
2022-03-08 16:53:40 -0600

    Range_method_loop

--- py_practice/Range_Loop.py
+++ py_practice/Range_Loop.py
@@ -4,3 +4,4 @@ for i in range(num):
     expenses.append(float(input("Enter an expense: ")))
 total_expenses = sum(expenses)
 print("Total Expenses for the entire week : $", total_expenses, sep='')
+#New comment to check the merge changes
